ac378c47458afa67b27edea219a5d987e7271c87
Jan Jelinek <kurnick@gmail.com>
2019-07-04 12:50:55 +0200

    Improvements and fixes
    * added support for config arguments
    * added support for response collecting
    * added support for foles with mock response per file
    * and maybe more, who knows

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 Initial idea was use Haskell to learn something new, but I start falling into lot of references,
 so I took something more familiar to me and with NodeJs it was done quickly.
 
-h2. What is going on?
+## What is going on?
 
 It allows you to mock some API responses and keep rest of API calls to original source. Some kind of proxy with partial mocking.
 What is important, you can mock data by configuration in `mock_response.json` file, or by passing specific headers.
@@ -15,10 +15,24 @@ What is important, you can mock data by configuration in `mock_response.json` fi
 Check your node version (`nvm use`) or see `.nvmrc`.
 Install all dependencies `yarn install` or `npm install`
 
+## Possible config arguments
+
+* `--apiUrl https://SOME_URL.`  specify original API which will be fetched instead of missing mocks
+* `--port 3000`                 specify port of running mock API
+* `--mockFile FILE_PATH`        specify file with mocks
+* `--mockAll`                   all missing mocks will be replaced by empty body response with 200 code, default is false (not provided argument)
+* `--collectMode`               Responses from all not mocked request will be stored to files in path where mockFile is located
+
 And run mock server
 
 ```
-node index.js
+//with default config
+$ node index.js
+```
+Or use custom config for run (you can run several instances at the same time with different API mocks)
+
+```
+$ node index.js --apiUrl https://api-staging.globalwebindex.com --port 3000 --mockFile mock_response.json
 ```
 
 Now you can change config for your API (PRO-NEXT for example) to `http://localhost:3000`
@@ -26,8 +40,9 @@ Now you can change config for your API (PRO-NEXT for example) to `http://localho
 
 Currently is everything hardcoded to original API url `https://api-staging.globalwebindex.com` and port `3000`
 
-What is good for me, you can easily change `mock_response.json` file on fly, so for example, when you need change for upcoming request, just edit file.
+You can easily change `mock_response.json` file on fly, so for example, when you need change for upcoming request, just edit file.
 Also there is exact match to API url so currently I let it filled with some working example `"forUrl": "/api/v2/query"`.
-As you can see, this place can be changed and no URL will be matched so it means all requests will be proxied to original destination.
+There is support for storing mocks in extra files per request. So you can choose between `responseData` or `responseFile`.
+See `mock_response.json` for examples.
 
 Important note about `mock_response.json`. If more URL will match, only first one will be taken. Probably, because I did not tested it :D
--- index.js
+++ index.js
@@ -1,25 +1,163 @@
-// content of index.js
 const http = require('http')
 const fs = require('fs')
 const requestModule = require('request')
+const process = require('process')
+const path = require('path');
 
-const port = 3000
-
-const GW_API_URL = 'https://api-staging.globalwebindex.com'
 const MOCK_CODE_HEADER = 'mock_responsecode'
 const MOCK_RESPONSE_HEADER = 'mock_response'
 
-const MOCK_FILE = './mock_response.json'
+const defaultHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'access-control-allow-headers': 'Authorization,content-type'
+}
+
+const config = (() => {
+  const [ , , ...args ] = process.argv
+  let defaultArgs = {
+    apiUrl: 'https://api-staging.globalwebindex.com',
+    port: 3000,
+    mockFile: './mock_response.json',
+    mockAll: false,
+    collectMode: false
+  }
+
+  let skipNext = false;
+  args.forEach((fullArg, i) => {
+    if (skipNext || fullArg.indexOf('--') === -1) return skipNext = false
+    let arg = fullArg.replace('--', '')
+
+    if (defaultArgs[arg] !== undefined) {
+      if (typeof defaultArgs[arg] === 'boolean') {
+        defaultArgs[arg] = true
+      } else if (args[i+1] !== undefined){
+        defaultArgs[arg] = args[i+1]
+        skipNext = true
+      } else {
+        console.log('Are you sure about this arg? ' + arg)
+      }
+    }
+  })
+
+  return defaultArgs
+})()
 
 
 const getMockFromFile = (request) => {
-  if (fs.existsSync((MOCK_FILE))) {
+  if (fs.existsSync(config.mockFile)) {
     console.log('Read mock data from file')
 
-    let fileMock = fs.readFileSync(MOCK_FILE, "utf8")
+    let fileMock = fs.readFileSync(config.mockFile, "utf8")
     fileMock = JSON.parse(fileMock)
 
-    return fileMock.find((mock) => mock.forUrl && mock.forUrl === request.url)
+    const mock = fileMock.find((mock) => mock.forUrl && mock.forUrl === request.url)
+
+    if (mock && mock.responseFile && mock.responseData) {
+      console.warn('You have specified "responseFile" and "responseData" for this url ' + request.url + '. What dou you realy want to use, heh?')
+    } else if (mock && mock.responseFile) {
+      console.log('Reading mock data from file ' + mock.responseFile)
+      try {
+        let responseFromFile = fs.readFileSync(path.dirname(config.mockFile) + '/' + mock.responseFile, "utf8")
+        mock.responseData = JSON.parse(responseFromFile)
+      } catch(e) {
+        console.error('Reading failed, are you sure about provided file?', e)
+      }
+    }
+
+    return mock
+  }
+}
+
+/**
+ * Proxy to original API url
+ */
+const proxy = (request, response) => {
+  console.log('Proxying to original API URL: ' + config.apiUrl + request.url)
+  let body = ''
+
+  if (request.headers['stop']) {
+    console.log('This is not funny, stop it');
+
+    request.on('data', chunk => {
+      console.log('data reading', chunk.toString())
+      body += chunk.toString(); // convert Buffer to string
+    });
+
+    response.end();
+    return;
+  }
+
+  let options = {
+    url: config.apiUrl + request.url,
+    method: request.method,
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': request.headers.authorization,
+      'Content-type': request.headers['content-type'] || 'application/json'
+    },
+    rejectUnauthorized: false
+  };
+
+  const proxyThis = (options, response) => {
+    let collectedBody = ''
+    const processThisRequest = ['OPTIONS', 'HEAD'].indexOf(request.method) === -1
+
+    requestModule(options, (error, res, body) => {
+      let headers = {
+        'Content-Type': 'application/json; charset=UTF-8', ...defaultHeaders
+      }
+      if (error) {
+        console.error('Request failed', error)
+      } else {
+        if (processThisRequest) {
+          console.log('Proxying finished.')
+          if (config.collectMode) {
+            console.log('Collect mode enabled, save response if not saved already')
+            try {
+              JSON.parse(body)
+              console.log('Got valid JSON response');
+
+              let fileName = path.dirname(config.mockFile) + '/' + request.url.replace(/[^a-z0-9]/ig, '_') + '.json'
+              if (!fs.existsSync(fileName)) {
+                fs.writeFile(fileName, body, (err) => {
+                  if(err) {
+                      return console.log(err);
+                  }
+                  console.log("The file with response was saved.");
+                });
+              }
+            } catch (e) {
+              console.log('Response is not valid JSON, ignoring', e, body)
+            }
+          }
+        } else {
+          console.log('Do not process this preflight request')
+          res = {statusCode: 200}
+          headers['Content-Type'] = 'text/plain; charset=utf-8'
+        }
+      }
+      response.writeHead(res.statusCode, headers)
+      response.end(body || "")
+    })
+  }
+
+  console.log('METHOD', request.method)
+
+  if (['POST', 'PUT'].indexOf(request.method) > -1) {
+    console.log('Prepare BODY')
+
+    request.on('data', chunk => {
+      body += chunk; // convert Buffer to string
+    });
+
+    request.on('end', () => {
+      console.log('Request body', body);
+      options.body = body
+      proxyThis(options, response)
+    })
+
+  } else {
+    proxyThis(options, response)
   }
 }
 
@@ -30,8 +168,9 @@ const requestHandler = (request, response) => {
   console.log('============== Processing ' + request.url + ' =================');
 
   const finishHim = () => {
-      response.writeHead(responseCode, {'Content-Type': 'application/json; charset=UTF-8', 'Access-Control-Allow-Origin': '*'})
-      console.log(request.url)
+      let headers = { ...defaultHeaders, 'Content-Type': 'application/json; charset=UTF-8'}
+      response.writeHead(responseCode, headers)
+      console.log('Mocking url: ' + request.url)
       response.end(JSON.stringify(responseJson))
   }
   const fileMock = getMockFromFile(request)
@@ -63,68 +202,20 @@ const requestHandler = (request, response) => {
     finishHim()
 
   } else {
-    console.log('Proxying to original GW API URL: ' + GW_API_URL + request.url)
-    let body = ''
-
-    if (request.headers['stop']) {
-      console.log('This is not funny, stop it');
-
-      request.on('data', chunk => {
-        console.log('data reading', chunk.toString())
-        body += chunk.toString(); // convert Buffer to string
-      });
-
-      response.end();
-      return;
-    }
-
-    var options = {
-      url: GW_API_URL + request.url,
-      method: request.method,
-      headers: request.headers,
-      rejectUnauthorized: false
-    }
-
-    const proxyThisShit = (options, response) => {
-      requestModule(options, (error, res, body) => {
-        if (error) {
-          console.error('Request failed', error)
-        } else {
-          console.log('Proxying finished, woohoo!')
-        }
-      }).pipe(response)
-    }
-
-    console.log('METHOD', request.method)
-
-    if (request.method === 'POST') {
-      console.log('Prepare BODY')
-
-
-      request.on('data', chunk => {
-        console.log('data reading')
-        body += chunk; // convert Buffer to string
-      });
-
-      request.on('end', () => {
-        console.log('Finish him', body);
-        options.body = body
-        proxyThisShit(options, response)
-      })
-
+    if (canMock && config.mockAll) {
+      finishHim()
     } else {
-      proxyThisShit(options, response)
+      proxy(request, response)
     }
-
   }
 }
 
 const server = http.createServer(requestHandler)
 
-server.listen(port, (err) => {
+server.listen(config.port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
 
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${config.port}`)
 })
